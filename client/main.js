a6bb65d0ce742b5a5953a76ce4b613eea354aaee
SuperGato <ledbrandon@yahoo.com>
2019-08-06 22:19:17 +0000

    store, read, and display data added data to server send data through endpoints change client address to correct server address put names on dom

--- client/main.js
+++ client/main.js
@@ -10,21 +10,32 @@ function getData(){
     url:'../server/getnames.json',
     method:'GET',
     dataType: 'json',
-    success: handleDataFromServer
+    success: handleDataNameFromServer
   }
   $.ajax(setting);
 }
 
 function getFood(){
   var setting = {
-    url:'../server/getFood.json',
+    url:'http://localhost:3000/getFood',
     method:'GET',
-    dataType: 'json',
-    success: handleDataFromServer
+    dataType: 'text',
+    success: handleDataFromServer,
+    error: function(error){
+      console.log('error')
+    }
   }
   $.ajax(setting);
 }
 
 function handleDataFromServer(response){
   console.log(response);
+}
+
+function handleDataNameFromServer(response) {
+  console.log(response);
+  for(var index = 0; index < response.length; index++){
+    var nameDiv = $('<div>').text(response[index].name);
+    $('body').append(nameDiv);
+  }
 }
--- server/server.js
+++ server/server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
 
+const names = [
+  {
+    "name": "Sam"
+  },
+  {
+    "name": "Jase"
+  }
+];
+
+const favFoods = [
+  {
+    "food": "ramen"
+  },
+  {
+    "age": "18"
+  }
+];
+
+
+server.use(cors());
+
+server.get('/getnames', function(request, respond){
+  respond.send(names);
+})
+
+server.get('/getFood', function(request, respond){
+  respond.send(favFoods);
+})
+
+server.listen(3000);
