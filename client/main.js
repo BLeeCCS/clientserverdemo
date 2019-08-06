77590701e3a5626ad06137dea62ca3356fedcd8b
SuperGato <ledbrandon@yahoo.com>
2019-08-06 19:58:22 +0000

    added fav foods

--- client/main.js
+++ client/main.js
@@ -2,11 +2,22 @@ $(document).ready(startApp);
 
 function startApp(){
   getData();
+  getFood();
 }
 
 function getData(){
   var setting = {
     url:'../server/getnames.json',
+    method:'GET',
+    dataType: 'json',
+    success: handleDataFromServer
+  }
+  $.ajax(setting);
+}
+
+function getFood(){
+  var setting = {
+    url:'../server/getFood.json',
     method:'GET',
     dataType: 'json',
     success: handleDataFromServer
